64aa14cd7739112d09decbd06df9a04084bc95ff
kirke <kirke@kirkenoMacBook-Air.local>
2022-04-10 16:03:34 +0900

    Indexのレイアウト調整　認証・未認証時のUIを変更

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
 import { Link } from "@remix-run/react";
 import { MdOutlineHowToVote } from "react-icons/md";
+import { useOptionalUser } from "~/utils";
+import { Form } from "@remix-run/react";
 
 const Header: FC = () => {
+  const user = useOptionalUser();
+
   return (
     <>
       <nav className="relative mb-3 flex flex-wrap items-center justify-between bg-amber-700 px-2 py-3">
@@ -12,7 +16,7 @@ const Header: FC = () => {
               className="mr-4 inline-block whitespace-nowrap py-2 text-sm font-bold uppercase leading-relaxed text-white"
               to="/"
             >
-              Scotch Review
+              美味いスコッチが飲みたい
             </Link>
           </div>
           <div
@@ -22,29 +26,62 @@ const Header: FC = () => {
             <ul className="flex list-none flex-col lg:ml-auto lg:flex-row">
               <li className="nav-item">
                 <Link
+                  to="scotch/tastings"
+                  className="flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-white hover:opacity-75"
+                >
+                  <MdOutlineHowToVote />
+                  <span className="ml-2">味見</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
                   to="scotch/"
                   className="flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-white hover:opacity-75"
                 >
                   <MdOutlineHowToVote />
-                  <span className="ml-2">Review</span>
+                  <span className="ml-2">生産地</span>
                 </Link>
               </li>
               <li className="nav-item">
                 <Link
-                  to="/join"
-                  className="mr-4 flex items-center justify-center rounded-md border border-transparent bg-white px-2 py-1 text-base font-medium text-blue-900 shadow-sm hover:bg-blue-50 sm:px-8"
+                  to="scotch/"
+                  className="flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-white hover:opacity-75"
                 >
-                  Sign up
+                  <MdOutlineHowToVote />
+                  <span className="ml-2">コンセプト</span>
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link
-                  to="/login"
-                  className="flex items-center justify-center rounded-md bg-blue-800 px-4 py-1 font-medium text-white hover:bg-blue-900 sm:px-8"
-                >
-                  Log In
-                </Link>
-              </li>
+              {user ? (
+                <li className="nav-item">
+                  <Form action="/logout" method="post">
+                    <button
+                      type="submit"
+                      className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                    >
+                      Logout
+                    </button>
+                  </Form>
+                </li>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      to="/join"
+                      className="mr-4 flex items-center justify-center rounded-md border border-transparent bg-white px-2 py-1 text-base font-medium text-blue-900 shadow-sm hover:bg-blue-50 sm:px-8"
+                    >
+                      Sign up
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/login"
+                      className="flex items-center justify-center rounded-md bg-blue-800 px-4 py-1 font-medium text-white hover:bg-blue-900 sm:px-8"
+                    >
+                      Log In
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -22,16 +22,14 @@ export default function Index() {
     <>
       <Header />
       <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-        {data.noteListItems.length === 0 ? (
-          <p className="p-4">No notes yet</p>
-        ) : (
-          <div>
+        {data.noteListItems.length === 0 ? null : (
+          <div className="mx-auto">
             {data.noteListItems.map((note) => (
               <li key={note.id}>📝 {note.title}</li>
             ))}
           </div>
         )}
-        <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+        {/* <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
           {user ? (
             <Link
               to="/notes"
@@ -40,22 +38,9 @@ export default function Index() {
               View Notes for {user.email}
             </Link>
           ) : (
-            <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-              <Link
-                to="/join"
-                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-              >
-                Sign up
-              </Link>
-              <Link
-                to="/login"
-                className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600  "
-              >
-                Log In
-              </Link>
-            </div>
+            null
           )}
-        </div>
+        </div> */}
       </main>
     </>
   );
