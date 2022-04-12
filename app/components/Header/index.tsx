de26a2f59fd0f951a7166382aa4d253fbfb82e00
kirke <kirke@kirkenoMacBook-Air.local>
2022-04-12 21:21:27 +0900

    commit

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -56,7 +56,7 @@ const Header: FC = () => {
                   <Form action="/logout" method="post">
                     <button
                       type="submit"
-                      className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                      className="rounded bg-slate-600 px-3 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
                     >
                       Logout
                     </button>
