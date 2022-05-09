234c1ef34bdc186396d0ad4726a1e4bc81b00ada
kirke <kirke@kirkenoMacBook-Air.local>
2022-05-10 06:32:04 +0900

    remake index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,16 +3,15 @@ import Header from "~/components/aspect/user/Header";
 import { useOptionalUser } from "~/utils";
 import { LoaderFunction } from "@remix-run/server-runtime";
 import { useLoaderData } from "@remix-run/react";
-
-import { getIndexNoteListItems } from "~/models/note.server";
+import { getIndexScotchListItems } from "~/models/scotch.server";
 
 type LoaderData = {
-  noteListItems: Awaited<ReturnType<typeof getIndexNoteListItems>>;
+  scotchListItems: Awaited<ReturnType<typeof getIndexScotchListItems>>;
 };
 
 export const loader: LoaderFunction = async () => {
-  const noteListItems = await getIndexNoteListItems();
-  return json<LoaderData>({ noteListItems });
+  const scotchListItems = await getIndexScotchListItems();
+  return json<LoaderData>({ scotchListItems });
 };
 export default function Index() {
   const data = useLoaderData() as LoaderData;
@@ -21,25 +20,13 @@ export default function Index() {
     <>
       <Header />
       <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-        {data.noteListItems.length === 0 ? null : (
+        {data.scotchListItems.length === 0 ? null : (
           <div className="mx-auto">
-            {data.noteListItems.map((note) => (
-              <li key={note.id}>📝 {note.title}</li>
+            {data.scotchListItems.map((scotch) => (
+              <li key={scotch.id}>📝 {scotch.bottleName}</li>
             ))}
           </div>
         )}
-        {/* <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-          {user ? (
-            <Link
-              to="/notes"
-              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-            >
-              View Notes for {user.email}
-            </Link>
-          ) : (
-            null
-          )}
-        </div> */}
       </main>
     </>
   );
