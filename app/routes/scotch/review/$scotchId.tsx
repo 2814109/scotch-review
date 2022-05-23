44ce0a596363ba33a8135491d27e3411df40fc01
kirke <kirke@kirkenoMacBook-Air.local>
2022-05-24 07:07:53 +0900

    create Create button

--- app/routes/scotch/review/$scotchId.tsx
+++ app/routes/scotch/review/$scotchId.tsx
@@ -12,7 +12,7 @@ import ReviewFormIsOpen from "~/state/review/atoms/ReviewFormIsOpen";
 import ReviewForm from "~/components/aspect/user/routes/review/ReviewForm";
 import ConfirmationForm from "~/components/aspect/user/routes/review/ConfirmationForm";
 import { useOptionalUser } from "~/utils";
-
+import CreateButton from "~/components/aspect/user/routes/review/CreateButton";
 type LoaderData = {
   scotch: Scotch;
 };
@@ -29,10 +29,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 const ScotchDetailPage: FC = () => {
   const data = useLoaderData() as LoaderData;
-
-  // TODO コンポーネント切り分けの際に状態管理をリファクタリング
-  const [isOpen, setIsOpen] = useRecoilState(ReviewFormIsOpen);
-
+  const isOpen = useRecoilValue(ReviewFormIsOpen);
   const user = useOptionalUser();
 
   return (
@@ -40,17 +37,7 @@ const ScotchDetailPage: FC = () => {
       <div className="sm:flex sm:justify-center">
         <h1>{data.scotch.bottleName}</h1>
       </div>
-      <div className="sm:flex sm:justify-center">
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-        >
-          <div className="flex items-center p-1">
-            <span className="mr-2">Create Review</span>
-            <MdOutlineHowToVote />
-          </div>
-        </button>
-      </div>
+      <CreateButton />
       {isOpen && (user ? <ReviewForm /> : <ConfirmationForm />)}
     </>
   );
