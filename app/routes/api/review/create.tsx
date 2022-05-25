a4b7e41c76f8f9c89bfd133890a5c4905e630a49
kirke <kirke@kirkenoMacBook-Air.local>
2022-05-25 09:30:58 +0900

    create review api

---
+++ app/routes/api/review/create.tsx
@@ -0,0 +1,51 @@
+import type { ActionFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { requireUserId } from "~/session.server";
+import { createReview } from "~/models/review.server";
+import { ScotchFormActionData } from "~/types/form/scotch";
+
+export const action: ActionFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+
+  const formData = await request.formData();
+  const star = Number(formData.get("star"));
+  const comment = String(formData.get("comment"));
+  const scotchId = String(formData.get("scotchId"));
+
+  // if (typeof bottleName !== "string" || bottleName.length === 0) {
+  //   return json<ScotchFormActionData>(
+  //     { errors: { bottleName: "Bottle Name is required" } },
+  //     { status: 400 }
+  //   );
+  // }
+
+  // if (typeof limited !== "string" || limited.length === 0) {
+  //   return json<ScotchFormActionData>(
+  //     { errors: { limited: "limited is required" } },
+  //     { status: 400 }
+  //   );
+  // }
+
+  // if (typeof price !== "number") {
+  //   return json<ScotchFormActionData>(
+  //     { errors: { price: "price is required" } },
+  //     { status: 400 }
+  //   );
+  // }
+
+  // if (typeof age !== "number") {
+  //   return json<ScotchFormActionData>(
+  //     { errors: { age: "age is required" } },
+  //     { status: 400 }
+  //   );
+  // }
+
+  await createReview({
+    star,
+    comment,
+    scotchId,
+    userId,
+  });
+
+  return redirect(`/admin/scotch/${scotchId}`);
+};
